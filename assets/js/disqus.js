2170c466da03c516bf9fdf2a5140a06301bb2fe8
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2021-09-26 20:42:03 +0200

    Alternative way

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -1,22 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let disqus = document.getElementById('disqus_thread');
-
-    let remove_ads = setInterval(() => {
-        let iframes = document.getElementsByTagName('iframe');
-
-        for (var iframe in iframes) {
-            if (typeof iframes[iframe].src === 'undefined') {
-                continue;
+(function($){
+    setInterval(() => {
+        $.each($('iframe'), (arr,x) => {
+            let src = $(x).attr('src');
+            if (src && src.match(/(ads-iframe)|(disqusads)/gi)) {
+                $(x).remove();
             }
-
-            if (iframes[iframe].src.match(/(ads-iframe)|(disqusads)/gi)) {
-                iframes[iframe].style.display = 'none';
-                disqus.style.width = '100%';
-            }
-        }
-    }, 500);
-
-    setTimeout(function () {
-        clearInterval(remove_ads);
-    }, 5000);
-});
+        });
+    }, 300);
+})(jQuery);
